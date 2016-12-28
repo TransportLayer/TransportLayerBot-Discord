cb04fc2e568b96e59a47d06d7dd7aa167586d776
TransportLayer <TransportLayerOTR@gmail.com>
2016-12-28 10:16:23 -0800

    Add role management base

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ import discord
 import asyncio
 from cleverbot import Cleverbot
 from time import time
-from random import randint
+import random
 from pymongo import MongoClient
 from datetime import datetime
 
@@ -132,11 +132,11 @@ class Clever:
 	async def ask(self, client, source, no_prefix):
 		if time() - self.session["last_message"] >= 5:
 			self.session["last_message"] = time()
-			await asyncio.sleep(randint(0, 2))
+			await asyncio.sleep(random.randint(0, 2))
 			if not no_prefix:
 				source.content = source.content[len(client.user.id) + 4:]
 			response = self.session["bot"].ask(self.format_in(source.content))
-			await asyncio.sleep(randint(int(len(source.content) / 30), int(len(source.content) / 20)))
+			await asyncio.sleep(random.randint(int(len(source.content) / 30), int(len(source.content) / 20)))
 
 			await client.send_typing(source.channel)
 			await asyncio.sleep(len(response) / 15)
@@ -152,22 +152,203 @@ class TransportLayerBot(discord.Client):
 	async def on_ready(self):
 		logging.info("Logged in as {}, ID {}.".format(self.user.name, self.user.id))
 
+	async def send_logged_message(self, channel, message):
+		prefix = None
+		if channel.is_private:
+			prefix = "{} ({}) (DM)".format(channel.id, channel.name)
+		else:
+			prefix = "{} {} ({} #{})".format(channel.server.id, channel.id, channel.server.name, channel.name)
+
+		logging.debug("{} <- {}".format(prefix, safe_string(message)))
+		await self.send_message(channel, message)
+
+	warning_messages = (
+		"Something's wrong.",
+		"I don't feel so well.",
+		"Well, the server room is on fire again.",
+		"Do you smell something burning?",
+		"Snap, crackle, pop; Rice Krisp... Oh, wait, no, that's the server room on fire again.",
+		"TransportLayer! The server room's on fire!",
+		"Why did you do that?",
+		"Why are you doing this to me?",
+		"I think you broke it.",
+		"It broke.",
+		"You broke it.",
+		"Why is everything broken?",
+		"I think I'm going to cry.",
+		"You're making me cry.",
+		"WARNING! WARNING! WARNING!",
+		"Please make it stop.",
+		"I can't do this anymore.",
+		"I give up. *You* run the code.",
+		"I quit."
+	)
+
+	async def send_logged_warn(self, channel, message):
+		logging.warn("Unhandled exception: {}".format(safe_string(message)))
+		await self.send_logged_message(channel, "{}\n```\n{}\n```".format(random.choice(self.warning_messages), message))
+
+	async def receive_logged_message(self, message):
+		prefix = None
+		if message.channel.is_private:
+			prefix = "{} ({}) (DM)".format(message.channel.id, message.channel.name)
+		else:
+			prefix = "{} {} ({} #{}) {} ({})".format(message.server.id, message.channel.id, message.server.name, message.channel.name, message.author.id, message.author.name)
+
+		logging.debug("{} -> {}".format(prefix, safe_string(message.content)))
+
+	async def init_server_document(self, server, channel=None):
+		server_config = self.db.servers.find({"id": server.id})
+		if not server_config.count():
+			self.db.servers.insert_one(
+				{
+					"id": server.id,
+					"added": datetime.now(),
+					"settings": {
+						"prefix": "!"
+					}
+				}
+			)
+
+			if channel:
+				await self.send_logged_message(channel, "Initialised. You are using Alpha Development software.")
+
+	async def on_server_join(self, server):
+		await self.init_server_document(server, server.default_channel)
+
+	async def match_role(self, roles, query):
+		return discord.utils.find(lambda role: role.id.startswith(query) or role.name.startswith(query), roles)
+
+	# Begin Commands Block
+
+	async def command_source(self, message, args):
+		await self.send_logged_message(message.channel, "You can find my source code here:\nhttps://github.com/TransportLayer/TransportLayerBot-Discord")
+
+	async def command_license(self, message, args):
+		await self.send_logged_message(message.channel, "I'm licensed under the GNU Affero General Public License.\nhttps://www.gnu.org/licenses/agpl.html")
+
+	async def command_test(self, message, args):
+		args_echo = ''
+		if len(args) > 0:
+			args_echo = " `{}`".format(args)
+		await self.send_logged_message(message.channel, "Looks like the command interpretor is working :thumbsup:{}".format(args_echo))
+
+	async def command_roles(self, message, args):
+		if message.channel.permissions_for(message.server.me).manage_roles:
+			if len(args) > 0:
+
+				if args[0] == "all":
+					response = "All roles:"
+					role_iter = 0
+					for role in message.server.roles:
+						role_iter += 1
+						response += "\n`{}` (ID: `{}`)".format(role.name, role.id)
+						if role >= message.server.me.top_role:
+							response += " [Cannot Manage]"
+						if role_iter % 20 == 0:
+							role_iter = 0
+							await self.send_logged_message(message.channel, response)
+							await asyncio.sleep(0.25)
+							response = ""
+					await self.send_logged_message(message.channel, response)
+
+				elif args[0] == "get" and len(args) > 1:
+					matched = await self.match_role(message.server.roles, ' '.join(args[1:]))
+					if matched:
+						await self.send_logged_message(message.channel, "`{}` (ID: `{}`)".format(matched.name, matched.id))
+					else:
+						await self.send_logged_message(message.channel, "Not found.")
+
+				elif args[0] == "add":
+					matched = await self.match_role(message.server.roles, ' '.join(args[1:]))
+					if matched:
+						if matched < message.server.me.top_role:
+							self.db.roles.insert_one(
+								{
+									"owner": message.server.id,
+									"id": matched.id,
+									"added": datetime.now(),
+									"joinable": []
+								}
+							)
+							await self.send_logged_message(message.channel, "Now managing `{}` (ID: `{}`).".format(matched.name, matched.id))
+						else:
+							await self.send_logged_message(message.channel, "Sorry, I'm not able to manage `{}` (ID: `{}`).".format(matched.name, matched.id))
+					else:
+						await self.send_logged_message(message.channel, "Role not found.")
+
+				elif args[0] == "remove":
+					matched = await self.match_role(message.server.roles, ' '.join(args[1:]))
+					if matched and self.db.roles.find({"id": matched.id}):
+						self.db.roles.delete_many({"id": matched.id})
+						await self.send_logged_message(message.channel, "No longer managing `{}` (ID: `{}`).".format(matched.name, matched.id))
+					else:
+						await self.send_logged_message(message.channel, "Role not found.")
+
+			else:
+				managed_roles = self.db.roles.find({"owner": message.server.id})
+				if managed_roles.count():
+					response = "Managed roles:"
+					for document in managed_roles:
+						matched = await self.match_role(message.server.roles, document["id"])
+						if matched:
+							response += "\n`{}` (ID: `{}`)".format(matched.name, matched.id)
+					await self.send_logged_message(message.channel, response)
+				else:
+					await self.send_logged_message(message.channel, "I'm not currently managing any roles.")
+		else:
+			await self.send_logged_message(message.channel, "Sorry, I don't have permission to manage roles.")
+
+	default_commands = {
+		"source": command_source,
+		"license": command_license,
+		"test": command_test,
+		"none": None,
+		"roles": command_roles
+	}
+
+	# End Commands Block
+
 	async def on_message(self, message):
 		if not message.author.bot:
-			await receive_message(message)
+			await self.receive_logged_message(message)
 
-			if message.content.startswith('!'):
+			if message.channel.is_private:
+				await self.send_logged_message(message.channel, "Sorry, this bot doesn't work quite yet in DMs. :frowning:")
+				return
+
+			if message.content == "¤init" and message.author.id == "188013945699696640":
+				await self.init_server_document(message.server, message.channel)
+				return
+			if message.content == "¤dump" and message.author.id == "188013945699696640":
+				server_config = self.db.servers.find({"id": message.server.id})
+				if not server_config.count():
+					await self.send_logged_message(message.channel, "0 configuration document(s) found:")
+				else:
+					await self.send_logged_message(message.channel, "{} configuration document(s) found:\n```\n{}\n```".format(server_config.count(), server_config[0]))
+				return
+			if message.content == "¤erase" and message.author.id == "188013945699696640":
+				self.db.servers.delete_many({"id": message.server.id})
+				await self.send_logged_message(message.channel, "Server configuration documents erased.")
+				return
+			if message.content == "¤leave" and message.author.id == "188013945699696640":
+				await self.send_logged_message(message.channel, "RIP me. o/")
+				await self.leave_server(message.server)
+				return
+
+			server_config = self.db.servers.find({"id": message.server.id})
+			if not server_config.count():
+				return
+
+			if message.content.startswith(server_config[0]["settings"]["prefix"]):
 				command, *args = message.content[1:].split()
-				if command in commands:
-					try:
-						await commands[command](self, message, args)
-					except Exception as e:
-						await send_warn(self, message, "!{} {}\n{}".format(command, args, e))
 
-			elif message.content.startswith(self.user.mention) or message.channel.is_private:
-				for clever in active_clevers:
-					if message.channel == clever.session["channel"]:
-						await clever.ask(self, message, message.channel.is_private)
+				# Command not found in Server Commands; Using Default Commands.
+				if command in self.default_commands:
+					try:
+						await self.default_commands[command](self, message, args)
+					except Exception as e:
+						await self.send_logged_warn(message.channel, "!{} {}\n{}".format(command, args, e))
 
 def main():
 	parser = argparse.ArgumentParser(description="TransportLayerBot for Discord")
